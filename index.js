23b8aa53efab76b87b110996960d2243f0e34282
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-08-12 21:56:41 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+const input = document.getElementById('button');
+
 function addingEventListener() {
+
+    input.addEventListener('click', function () {
+        alert('I was clicked!')
+    })
+
 }
+
